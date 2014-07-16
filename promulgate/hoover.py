dc39fc73cb4031a1952b6e91e15700c7b3ec5174
Mustafa <mus@musalbas.com>
2014-07-16 13:44:29 +0100

    init probe request hoover - can currently pick up probe request packets and determine its source address

---
+++ promulgate/hoover.py
@@ -0,0 +1,39 @@
+import thread
+import netaddr
+import socket
+
+SUBTYPE_PROBE_REQUEST = 0b01000000
+TYPE_MANAGEMENT       = 0b00000000
+
+class Hoover:
+
+    def __init__(self, interface):
+        self._rawsock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
+            socket.htons(0x0003))
+        self._rawsock.bind((interface, 0x0003))
+
+        thread.start_new(self._receiver, ())
+
+    def _read_probe_request_packet(self, rawpacket):
+        if ord(rawpacket[26]) != SUBTYPE_PROBE_REQUEST | TYPE_MANAGEMENT:
+            return False
+
+        packetinfo = {}
+
+        packetinfo['source_mac'] = rawpacket[36:42]
+
+        source_org = netaddr.EUI(packetinfo['source_mac'].encode('hex'))
+        source_org = source_org.oui.registration().org
+        packetinfo['source_org'] = source_org
+
+        return packetinfo
+
+    def _receiver(self):
+        while True:
+            rawpacket = self._rawsock.recvfrom(2048)
+            packetinfo = self._read_probe_request_packet(rawpacket[0])
+
+            if not packetinfo:
+                continue
+
+            print packetinfo
